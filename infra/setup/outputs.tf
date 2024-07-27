db49f7e214a2a4b15a447246f262bbe89f0dc35c
charan <sadhucharan994@gmail.com>
2024-07-27 23:49:28 +0530

    ecr resource and policy

--- infra/setup/outputs.tf
+++ infra/setup/outputs.tf
@@ -8,3 +8,14 @@ output "cd_user_access_key_secret" {
   description = "AWS key secret for CD user"
   sensitive   = true
 }
+
+
+output "ecr_repo_app" {
+  description = "ecr repository url for app image"
+  value       = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repo_proxy" {
+  description = "ecr repository url for app image"
+  value       = aws_ecr_repository.proxy.repository_url
+}
