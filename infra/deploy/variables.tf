cc8670474c499e3670d75dacebcdeb96859efeda
charan <sadhucharan994@gmail.com>
2024-08-15 13:15:14 +0530

    updated database.tf file

--- infra/deploy/variables.tf
+++ infra/deploy/variables.tf
@@ -12,3 +12,12 @@ variable "project" {
   description = "project name for tagging resources"
   default     = "reciepe-app-api"
 }
+
+variable "db_username" {
+  description = "Username for the recipe app api database"
+  default     = "recipeapp"
+}
+
+variable "db_password" {
+  description = "Password for the Terraform database"
+}
