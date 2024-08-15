c8e766c32ea76c5e6fc146cf4f873790abd715b8
charan <sadhucharan994@gmail.com>
2024-08-15 18:04:09 +0530

    updated servicelinked role in iam.tf file

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -190,3 +190,27 @@ resource "aws_iam_user_policy_attachment" "rds" {
   user       = aws_iam_user.cd.name
   policy_arn = aws_iam_policy.rds.arn
 }
+
+data "aws_iam_policy_document" "service_linked_rds" {
+  statement {
+    effect    = "Allow"
+    actions   = ["iam:CreateServiceLinkedRole"]
+    resources = ["arn:aws:iam::*:role/aws-service-role/rds.amazonaws.com/AWSServiceRoleForRDS"]
+    condition {
+      test     = "StringLike"
+      variable = "iam:AWSServiceName"
+      values   = ["rds.amazonaws.com"]
+    }
+  }
+}
+ 
+resource "aws_iam_policy" "service_linked_rds" {
+  name        = "${aws_iam_user.cd.name}-service_linked_rds"
+  description = "Allow Amazon RDS to call AWS services on behalf of your DB instances."
+  policy      = data.aws_iam_policy_document.service_linked_rds.json
+}
+ 
+resource "aws_iam_user_policy_attachment" "service_linked_rds" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.service_linked_rds.arn
+}
